214e69a0e90088dd06fc19d5b1790b37d1f2bf32
may-santos <mayara.lima@grupoboticario.com.br>
2025-09-24 13:25:04 -0300

    feat: implement transaction modal component with HTML, SCSS, and TypeScript

---
+++ src/app/features/transactions/presentation/components/transaction-modal/transaction-modal.component.ts
@@ -0,0 +1,56 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Transaction } from '../../../domain/transactions.model';
+
+@Component({
+  selector: 'app-transaction-modal',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './transaction-modal.component.html',
+  styleUrls: ['./transaction-modal.component.scss'],
+})
+export class TransactionModalComponent {
+  _isVisible = false;
+  
+  @Input() 
+  set isVisible(value: boolean) {
+    this._isVisible = value;
+    console.log('Modal isVisible mudou para:', value, new Date().toISOString());
+  }
+  
+  get isVisible(): boolean {
+    return this._isVisible;
+  }
+  
+  @Input() isSaving = false;
+  @Output() close = new EventEmitter<void>();
+  @Output() save = new EventEmitter<Transaction>();
+
+  newTransaction: Transaction = {
+    id: '',
+    createdAt: new Date(),
+    amount: 0,
+    type: 'WITHDRAW',
+  };
+
+  onSave() {
+    this.newTransaction.id = Math.random().toString(36).substring(7);
+    this.save.emit(this.newTransaction);
+    this.onClose();
+  }
+
+  onClose() {
+    this.close.emit();
+    this.resetForm();
+  }
+
+  resetForm() {
+    this.newTransaction = {
+      id: '',
+      createdAt: new Date(),
+      amount: 0,
+      type: 'WITHDRAW',
+    };
+  }
+}
