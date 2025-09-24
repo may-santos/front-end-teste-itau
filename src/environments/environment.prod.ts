c177231a76fadaa2d2ab8ff42f518d45e2cbe14b
may-santos <mayara.lima@grupoboticario.com.br>
2025-09-24 13:30:05 -0300

    feat: add environment configuration files for production and development

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  auth: {
+    domain: '',
+    clientId: '',
+    redirectUri: 'http://localhost:4200',
+    audience: 'https://teste-itau.com/api/v2',
+  },
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: false,
+  auth: {
+    domain: '',
+    clientId: '',
+    redirectUri: 'http://localhost:4200',
+    audience: 'https://teste-itau.com/api/v2',
+  },
+};
