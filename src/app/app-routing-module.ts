1bcff08757e32703c3370201529f26ad4052e382
may-santos <mayara.lima@grupoboticario.com.br>
2025-09-24 13:26:31 -0300

    feat: define application routes and implement AuthGuard for transaction access

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/auth/auth.guard';
 
-const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    redirectTo: '/transactions', 
+    pathMatch: 'full' 
+  },
+  {
+    path: 'transactions',
+    loadComponent: () => import('./features/transactions/presentation/pages/transactions-page.component').then(m => m.TransactionsPageComponent),
+    canActivate: [AuthGuard]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
