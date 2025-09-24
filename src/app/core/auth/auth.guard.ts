00b60bbaf70562691ac840a99ff3bfab4e8bcaf1
may-santos <mayara.lima@grupoboticario.com.br>
2025-09-24 14:45:54 -0300

    refactor: simplify AuthGuard's canActivate method for improved readability and functionality

--- src/app/core/auth/auth.guard.ts
+++ src/app/core/auth/auth.guard.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-
   constructor(private auth: AuthService) {}
 
-  canActivate(
-    next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return this.auth.isAuthenticated$;
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
+    return this.auth.isAuthenticated$.pipe(
+      take(1),
+      map((isAuth) => {
+        if (!isAuth) {
+          console.log('Usuário não autenticado');
+        }
+        return isAuth;
+      })
+    );
   }
 }
